0c6c26bc180e9a93f32ec022de88e3032d328a15
Mingzhe Zou <zoumingzhe@qq.com>
2025-02-19 15:33:27 +0800

    add configure

--- xpw/__init__.py
+++ xpw/__init__.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 
+from .configure import Argon2Config  # noqa:F401
 from .ldapauth import LdapAuth  # noqa:F401
 from .ldapauth import LdapInit  # noqa:F401
 from .password import Argon2Hasher  # noqa:F401
---
+++ xpw/configure.py
@@ -0,0 +1,72 @@
+# coding:utf-8
+
+from typing import Any
+from typing import Dict
+
+from toml import load
+
+from .password import Argon2Hasher
+
+CONFIG_DATA_TYPE = Dict[str, Any]
+DEFAULT_CONFIG_FILE = ".pwauth"
+
+
+class BasicConfig():
+    def __init__(self, datas: CONFIG_DATA_TYPE):
+        self.__datas: CONFIG_DATA_TYPE = datas
+
+    @property
+    def datas(self) -> CONFIG_DATA_TYPE:
+        return self.__datas
+
+    @classmethod
+    def loadf(cls, path: str = DEFAULT_CONFIG_FILE) -> CONFIG_DATA_TYPE:
+        """load config from toml file"""
+        return load(path)
+
+
+class Argon2Config(BasicConfig):
+    TYPE = "argon2"
+
+    def __init__(self, datas: CONFIG_DATA_TYPE):
+        datas.setdefault(self.TYPE, {})
+        datas.setdefault("users", {})
+        super().__init__(datas)
+
+    def __getitem__(self, user: str) -> Argon2Hasher:
+        return self.generate(self.datas["users"][user])
+
+    @property
+    def time_cost(self) -> int:
+        return self.datas[self.TYPE].get("time_cost", Argon2Hasher.DEFAULT_TIME_COST)  # noqa:E501
+
+    @property
+    def memory_cost(self) -> int:
+        return self.datas[self.TYPE].get("memory_cost", Argon2Hasher.DEFAULT_MEMORY_COST)  # noqa:E501
+
+    @property
+    def parallelism(self) -> int:
+        return self.datas[self.TYPE].get("parallelism", Argon2Hasher.DEFAULT_PARALLELISM)  # noqa:E501
+
+    @property
+    def hash_len(self) -> int:
+        return self.datas[self.TYPE].get("hash_length", Argon2Hasher.DEFAULT_HASH_LENGTH)  # noqa:E501
+
+    @property
+    def salt_len(self) -> int:
+        return self.datas[self.TYPE].get("salt_length", Argon2Hasher.DEFAULT_SALT_LENGTH)  # noqa:E501
+
+    @property
+    def salt(self) -> str:
+        return self.datas[self.TYPE].get("salt", None)
+
+    def generate(self, password: str) -> Argon2Hasher:
+        return Argon2Hasher(password) if password.startswith("$") else self.encode(password)  # noqa:E501
+
+    def encode(self, password: str) -> Argon2Hasher:
+        return Argon2Hasher.hash(password=password, salt=self.salt,
+                                 time_cost=self.time_cost,
+                                 memory_cost=self.memory_cost,
+                                 parallelism=self.parallelism,
+                                 hash_len=self.hash_len,
+                                 salt_len=self.salt_len)
