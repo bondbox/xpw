55974b5aef67d8e02a70881a76c7894d4ee29b50
Mingzhe Zou <zoumingzhe@qq.com>
2025-05-14 12:48:29 +0800

    authorize: improve TokenAuth class to support multi-user

--- xpw/authorize.py
+++ xpw/authorize.py
@@ -1,8 +1,8 @@
 # coding:utf-8
 
+from typing import Dict
 from typing import Optional
 
-from xpw.attribute import __project__
 from xpw.configure import Argon2Config
 from xpw.configure import BasicConfig
 from xpw.configure import DEFAULT_CONFIG_FILE
@@ -22,29 +22,33 @@ class TokenAuth():
     def config(self) -> BasicConfig:
         return self.__config
 
-    def delete_token(self, token: str) -> None:
-        if token in self.config.datas[self.SECTION]:
-            del self.config.datas[self.SECTION][token]
-        assert token not in self.config.datas[self.SECTION]
+    @property
+    def tokens(self) -> Dict[str, str]:
+        return self.config.datas[self.SECTION]
 
-    def update_token(self, token: str, note: Optional[str] = None) -> None:
-        self.config.datas[self.SECTION][token] = note or __project__
+    def delete_token(self, code: str) -> None:
+        if code in (tokens := self.tokens):
+            del tokens[code]
+        assert code not in self.tokens
 
-    def generate_token(self, note: Optional[str] = None) -> str:
+    def update_token(self, code: str, user: str = "") -> None:
+        self.tokens[code] = user
+
+    def generate_token(self, user: str = "") -> str:
         from xpw.password import Pass  # pylint:disable=import-outside-toplevel
 
         secret: Pass = Pass.random_generate(64, Pass.CharacterSet.ALPHANUMERIC)
-        self.update_token(token := secret.value, note)
-        return token
+        self.update_token(code := secret.value, user)
+        return code
 
     def password_verify(self, username: str, password: Optional[str] = None) -> Optional[str]:  # noqa:E501
         raise NotImplementedError()
 
-    def token_verify(self, token: str) -> Optional[str]:
-        return self.config.datas[self.SECTION].get(token)
+    def token_verify(self, code: str) -> Optional[str]:
+        return self.tokens.get(code)
 
     def verify(self, k: str, v: Optional[str] = None) -> Optional[str]:
-        if k == "":
+        if k == "":  # no available username, verify token
             assert isinstance(v, str)
             return self.token_verify(v)
 
