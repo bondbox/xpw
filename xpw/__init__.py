a193e6b2d914d138a07f5d560f2283c5bc6caa89
Mingzhe Zou <zoumingzhe@qq.com>
2025-08-08 17:07:18 +0800

    add import

--- xpw/__init__.py
+++ xpw/__init__.py
@@ -13,6 +13,8 @@ from xpw.configure import DEFAULT_CONFIG_FILE  # noqa:F401
 from xpw.ldapauth import LdapClient  # noqa:F401
 from xpw.ldapauth import LdapInit  # noqa:F401
 from xpw.password import Argon2Hasher  # noqa:F401
+from xpw.password import CharacterSet  # noqa:F401
+from xpw.password import Characters  # noqa:F401,H306
 from xpw.password import Pass  # noqa:F401
 from xpw.password import Salt  # noqa:F401
 from xpw.password import Secret  # noqa:F401
