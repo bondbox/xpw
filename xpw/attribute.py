c60271f1c11d9a5deedf3ce7a921e646f36b17d3
Mingzhe Zou <zoumingzhe@qq.com>
2025-11-03 11:23:06 +0800

    bump version from 0.14.1 to 0.14.2

--- xpw/attribute.py
+++ xpw/attribute.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 
 __project__ = "xpw"
-__version__ = "0.14.1"
+__version__ = "0.14.2"
 __urlhome__ = "https://github.com/bondbox/xpw/"
 __description__ = "Password management and authentication"
 
